3a5cbb45537a2952b0a86cef60be67b3706a0da7
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2020-11-09 20:30:19 -0500

    Add accum() fixture for Chapter 6

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -9,40 +9,43 @@ import pytest
 
 from stuff.accum import Accumulator
 
+# Fixtures
+
+
+@pytest.fixture
+def accum():
+    return Accumulator()
+
+
 # Tests
 
 
-def test_accumulator_init():
+def test_accumulator_init(accum):
     """Verify that new Accumulator instance has a starting count of 0"""
-    accum = Accumulator()
     assert accum.count == 0
 
 
-def test_accumulator_add_one():
+def test_accumulator_add_one(accum):
     """Verify that, by default, add() adds 1 to the accumulator count"""
-    accum = Accumulator()  # Arrange
     accum.add()  # Act
     assert accum.count == 1  # Assert
 
 
-def test_accumulator_add_three():
+def test_accumulator_add_three(accum):
     """Verify that add(3) adds 3 to the accumulator count"""
-    accum = Accumulator()
     accum.add(3)
     assert accum.count == 3
 
 
-def test_accumulator_add_twice():
+def test_accumulator_add_twice(accum):
     """Verify that the count increases appropriately w. multiple add() calls"""
-    accum = Accumulator()
     accum.add()
     accum.add()
     assert accum.count == 2
 
 
-def test_accumulator_cannot_set_count_directly():
+def test_accumulator_cannot_set_count_directly(accum):
     """Verify that the count attribute cannot be assigned directly"""
-    accum = Accumulator()
     # with pytest.raises(AttributeError, match=r"can't set attribute") as e:
     with pytest.raises(AttributeError, match=r"can't set attribute"):
         accum.count = 10
